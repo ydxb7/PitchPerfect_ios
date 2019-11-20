bf18ccb8de91a66c71bd246bb70818568991517d
Yini <ydxb7@mail.missouri.edu>
2019-11-19 18:24:40 -0800

    Initial Commit

---
+++ PitchPerfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PitchPerfect
+//
+//  Created by Yini Duanmu on 11/19/19.
+//  Copyright © 2019 Yini Duanmu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
