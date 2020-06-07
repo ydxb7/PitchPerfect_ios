736c32d82d9d1db1b7def26a40fff062dec8ffad
Yini <ydxb7@mail.missouri.edu>
2020-06-07 15:22:20 -0700

    add tag to each button, and use tag distinguish the buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -30,11 +30,25 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("play sound button")
+        switch ButtonType(rawValue: sender.tag)! {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("stop audio button pressed")
+        stopAudio()
     }
     
     override func viewDidLoad() {
