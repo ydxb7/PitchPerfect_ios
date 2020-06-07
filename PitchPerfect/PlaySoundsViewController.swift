071554bc025fe7c9d70ef8b340897ea7ae962a2d
Yini <ydxb7@mail.missouri.edu>
2020-06-06 19:30:06 -0700

    create IBOutlet and IBAction for all buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
     var recordedAudioURL: URL!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("play sound button")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("stop audio button pressed")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
